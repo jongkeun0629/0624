fa3479fe4108010dbf911f665b852a9ac65d7377
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-24 14:33:47 +0900

    New I/O

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,55 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 public class Main {
     public static void writeFile(String filename, String content){
+        Path filePath = Paths.get(filename);
 
-        // 기존 방식. finally로 파일 닫아주어야 함
-//        FileWriter writer = null;
-//        try {
-//            writer = new FileWriter(filename);
-//            writer.write(content);
-//        } catch (IOException e){
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                if (writer != null) writer.close();
-//            } catch (IOException e){
-//                e.printStackTrace();
-//            }
-//        }
+        try (FileChannel writeChannel = FileChannel.open(
+                filePath,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE)){
 
-        // try-with-resources. 파일 자동으로 닫는다
-        try (FileWriter writer = new FileWriter(filename, true)) {      // 두 번째 인자 true. 이어쓰기
-            writer.write(content);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public static void readFile(String filename){
-        try (BufferedReader reader= new BufferedReader(new FileReader(filename))){
-            String line;
-
-            // 마지막줄까지
-            while((line = reader.readLine()) != null){
-                System.out.println(line);
-            }
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            buffer.put(content.getBytes());
+            buffer.flip();
+            writeChannel.write(buffer);
+            System.out.println("파일 작성 완료");
         } catch (IOException e){
             e.printStackTrace();
         }
     }
 
+    public static void readFile(String filename){
+        Path filePath = Paths.get(filename);
 
+        try (FileChannel readChannel = FileChannel.open(filePath, StandardOpenOption.READ)) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            int bytesRead = readChannel.read(buffer);
+
+            while (bytesRead != -1){
+                buffer.flip();
+
+                String chunk = StandardCharsets.UTF_8.decode(buffer).toString();
+                System.out.print(chunk);
+
+                buffer.clear();
+                bytesRead = readChannel.read(buffer);
+            }
+            System.out.println("\n읽기 완료");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
-//        writeFile("example.txt", "\n이어쓰기");
-//        System.out.println("파일 생성 완료");
-
-        readFile("example.txt");
+//        writeFile("writeTest.txt", "뭐 먹지?");
+        readFile("writeTest.txt");
     }
 }
